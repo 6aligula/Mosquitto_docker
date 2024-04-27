d1a144b3daaa3bfd0bb2e31fdf4af0c1e7806657
Vinicio <calamot51@gmail.com>
2024-04-27 18:18:51 +0200

    add endpoints for turn on and off dispenser

---
+++ flask/app.py
@@ -0,0 +1,112 @@
+from flask import Flask, jsonify
+import paho.mqtt.client as mqtt
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Carga las variables de entorno desde el archivo .env
+load_dotenv()
+
+app = Flask(__name__)
+
+# Configura los detalles del broker MQTT
+mqtt_broker = os.getenv("MQTT_BROKER")
+mqtt_port = 1883
+dispensar_topic = os.getenv("DISP_TOPIC")
+soplar_topic = os.getenv("SOPLAR_TOPIC")
+mqtt_username = os.getenv("MQTT_USERNAME")
+mqtt_password = os.getenv("MQTT_PASSWORD")
+
+# Inicializa el cliente MQTT
+client = mqtt.Client()
+
+# Establece el username y password si están disponibles
+if mqtt_username and mqtt_password:
+    client.username_pw_set(mqtt_username, mqtt_password)
+
+# Configuración de MongoDB modificada para usar variables de entorno
+mongo_user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+mongo_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+mongo_host = os.getenv("MONGODB_HOST", "localhost")
+mongo_port = os.getenv("MONGODB_PORT", "27017")
+mongo_uri = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/"
+
+mongo_client = MongoClient(mongo_uri)
+db = mongo_client['dispensador_database']  # Nombre de la base de datos
+dispensar_collection = db['dispensar']  # Colección para dispensar
+soplar_collection = db['soplar']  # Colección para soplar
+
+# Callback para cuando el cliente recibe una CONNACK del servidor
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+        client.subscribe(dispensar_topic)
+        client.subscribe(soplar_topic)
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+
+# Callback que se llama cuando se recibe un mensaje del servidor.
+def on_message(client, userdata, msg):
+    payload = msg.payload.decode('utf-8')
+    try:
+        if msg.topic == dispensar_topic:
+            print(msg.topic + " " + payload)
+            dispensar_collection.insert_one({"dispensar": int(payload)})
+        elif msg.topic == soplar_topic:
+            print(msg.topic + " " + payload)
+            soplar_collection.insert_one({"soplar": int(payload)})
+    except Exception as e:
+        print(f"Failed to insert data: {e}")
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(mqtt_broker, mqtt_port, 60)
+client.loop_start()
+
+@app.route('/')
+def index():
+    return "MQTT to Flask Bridge"
+
+@app.route('/dispensar/encender', methods=['POST'])
+def encender_dispensar():
+    try:
+        client.publish(dispensar_topic, '1')
+        return jsonify({"success": True, "message": "Dispensador encendido"}), 200
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 500
+
+@app.route('/dispensar/apagar', methods=['POST'])
+def apagar_dispensar():
+    try:
+        client.publish(dispensar_topic, '0')
+        return jsonify({"success": True, "message": "Dispensador apagado"}), 200
+    except Exception as e:
+        return jsonify({"success": False, "message": str(e)}), 500
+
+
+@app.route('/getDispensar')
+def get_dispensar():
+    try:
+        dispensario = dispensar_collection.find().sort("_id", -1).limit(10)
+        # Convertir cada ObjectId a string
+        result = [{"dispensar": disp["dispensar"], "id": str(disp["_id"])} for disp in dispensario]
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/getSoplar')
+def get_soplar():
+    try:
+        # Recupera los últimos 10 registros de soplar
+        soplidos = soplar_collection.find().sort("_id", -1).limit(10)
+        result = [{"soplar": sopla["soplar"], "id": str(sopla["_id"])} for sopla in soplidos]
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
